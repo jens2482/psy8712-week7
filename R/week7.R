0e3f6f1702727b86ea025f1e3bb4d8fa3ae3ab63
Amanda Jensen <jens2482@umn.edu>
2024-03-10 17:08:58 -0500

    Figs 1-3

--- R/week7.R
+++ R/week7.R
@@ -1,12 +1,12 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-
+library(GGally)
 
 
 
 # Data Import and Cleaning
-timeSpent <- read_csv ("../data/week3.csv") %>%
+week7_tbl <- read_csv ("../data/week3.csv") %>%
   mutate(timeStart = ymd_hms (timeStart)) %>%
   mutate(timeEnd = ymd_hms (timeEnd)) %>%
   mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control"))) %>%
@@ -17,3 +17,27 @@ timeSpent <- read_csv ("../data/week3.csv") %>%
 
 
 
+
+# Visualization
+{week7_tbl %>% 
+    ggpairs(columns = 5:13,lower = list(continuous = "points"), diag = list(continuous = "densityDiag"), upper = list(continuous = "cor"))} %>%
+  ggsave(filename = "../figs/fig0.png", ., height=5, width=10, dpi=600) #unclear whether or not this needed to be saved as a figure; did just in case as fig0
+{week7_tbl %>% 
+    ggplot(aes(timeStart, q1))+
+    geom_point()+
+    xlab("Date of Experiment")+
+    ylab("Q1 Score")} %>%
+  ggsave(filename = "../figs/fig1.png", ., height=3, width=6, units="in", dpi=600)
+{week7_tbl %>%  
+    ggplot(aes(q1,q2, color = gender))+
+    geom_jitter()+
+    labs(color = "Participant Gender")} %>%
+  ggsave(filename = "../figs/fig2.png", ., height=3, width=6, units="in", dpi=600)
+{week7_tbl %>%  
+    ggplot(aes(q1,q2))+
+    geom_jitter()+
+    facet_wrap(~gender) +    
+  xlab("Score on Q1")+
+    ylab("Score on Q2")} %>%
+  ggsave(filename = "../figs/fig3.png", ., height=3, width=6, units="in", dpi=600)
+  
